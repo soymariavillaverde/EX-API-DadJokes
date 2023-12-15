6488f66ad6db27488d52ab647117d548c5369b81
soymariavillaverde <soymariavillaverde@gmail.com>
2023-12-15 14:37:36 +0100

    API call refactored :recycle:

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const jokeBtn = document.getElementById('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
-// USING ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
@@ -11,23 +10,11 @@ async function generateJoke() {
     },
   }
 
-  const response = await fetch('https://icanhazdadjoke.com', config)
-  const data = await response.json()
+  const endPoint = "https://icanhazdadjoke.com";
+  let response = await fetch(endPoint , config);
+  let data = await response.json();
   jokeEl.innerHTML = data.joke
+
 }
 
-//   HINTS
-// USING .then()
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
-
-//   >>fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
-// }
+generateJoke();
